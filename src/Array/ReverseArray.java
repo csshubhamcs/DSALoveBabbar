30ff8bcfebe1ed11ccd6ea785eab57e75e35d028
csshubhamcs <shubhamsinghhrx@gmail.com>
2022-12-27 17:23:03 +0530

    Reverse Array

---
+++ src/Array/ReverseArray.java
@@ -0,0 +1,10 @@
+package Array;
+
+public class ReverseArray {
+    public static void main(String[] args) {
+        int arr[] = {2,8,3,7,5,9,1};
+        for (int i= arr.length-1;i>=0;i--){
+            System.out.print(arr[i]+",");
+        }
+    }
+}
